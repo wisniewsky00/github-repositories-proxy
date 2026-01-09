f90e750eec12bf19ab199011141715affd2670c9
wisniewsky00 <krystian.wisniewski2906@gmail.com>
2026-01-09 10:42:04 +0100

    feat: add basic RestClient configuration and GitHub token property

---
+++ src/main/java/com/github/wisniewsky00/github_repositories_proxy/RestClientConfig.java
@@ -0,0 +1,29 @@
+package com.github.wisniewsky00.github_repositories_proxy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import static org.springframework.web.client.RestClient.*;
+
+@Configuration
+class RestClientConfig {
+
+    @Value("${github.personal.access.token}")
+    private String githubPersonalAccessToken;
+
+    @Bean
+    RestClient restClient() {
+
+            if (githubPersonalAccessToken == null) {
+                throw new IllegalStateException("GitHub personal access token cannot be null");
+            }
+
+            return builder()
+                    .baseUrl("https://api.github.com")
+                    .defaultHeader("Accept", "application/vnd.github+json")
+                    .defaultHeader("Authorization", "Bearer " + githubPersonalAccessToken)
+                    .build();
+    }
+}
